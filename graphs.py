dd505b8b586d1671759f7f7670880a8aaf31b3e7
iamthewalrus67 <bodya.ru02@gmail.com>
2020-12-30 15:16:49 +0200

    first commit

---
+++ graphs.py
@@ -0,0 +1,10 @@
+def read_graph_from_file(path):
+    edges = []
+    with open(path, 'r') as vertices:
+        for vertex in vertices:
+            edge = vertex.strip().split(',')
+            edges.append(tuple(edge))
+    return edges
+
+
+print(read_graph_from_file('vertices.csv'))
