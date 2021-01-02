1961072c1d9b6dac1c6c06c04809283a2c1ad3e9
Roman Kulyk <roman.kulyk@ucu.edu.ua>
2021-01-02 18:33:11 +0200

    fixed bipartite_check

--- graphs.py
+++ graphs.py
@@ -132,31 +132,33 @@ def hamiltonian_cycle(graph: list, directed=False) -> list:
 def bipartite_check(graph: list) -> bool:
     """
     Return True if given graph is bipartite and False if not.
-
-    >>> bipartite_check([(1, 2), (3, 2), (4, 2), (1, 5), (3, 5), (4, 2)])
+    >>> bipartite_check([(5, 1), (1, 1), (2, 1), (3, 2), (4, 5), (1, 2), (5, 5)])
     False
-    >>> bipartite_check([(1, 4), (2, 3), (1, 2), (3, 4)])
+    >>> bipartite_check([(1, 2), (1, 4), (2, 1), (2, 3), (3, 2), (3, 4), (4, 1)])
     True
+    >>> bipartite_check([(1, 2), (1, 3), (2, 1), (2, 3), (3, 2), (3, 1)]) 
+    False
     >>> bipartite_check([])
     True
     """
+    if graph == []:
+        return True
     matrix = get_adjancy_matrix(graph)
     vertices = len(matrix)
-
-    for i in range(vertices):
-        # checking diagonals not to contain self-loops
-        if matrix[i][i] == 1:
+    colorArr = [-1] * vertices
+    colorArr[0] = 1
+    queue = []
+    queue.append(0)
+    while queue:
+        u = queue.pop()
+        if matrix[u][u] == 1:
             return False
-
-        edges_amount = 0
-
-        for k in range(vertices):
-            # counting amount of edges
-            if matrix[i][k] == 1:
-                edges_amount = edges_amount + 1
-        # checking number of edges and returning False if it is odd
-        if edges_amount % 2 != 0:
-            return False
+        for v in range(vertices):
+            if matrix[u][v] == 1 and colorArr[v] == -1:
+                colorArr[v] = 1 - colorArr[u]
+                queue.append(v)
+            elif matrix[u][v] == 1 and colorArr[v] == colorArr[u]:
+                return False
     return True
 
 
