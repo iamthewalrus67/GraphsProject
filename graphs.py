22e6e72f39cfc7a80b631af564ccacbb4739bbdb
oleksadobush <oleksandra.stasiuk@ucu.edu.ua>
2021-01-02 19:20:37 +0200

    added eulerian_circuit

--- graphs.py
+++ graphs.py
@@ -245,3 +245,84 @@ def colour_graph(matrix: list, vertices: list) -> dict:
     list_vertex_colour = [(name_of_vertex, list_colours[index])
                           for index, name_of_vertex in enumerate(vertices)]
     return list_vertex_colour
+
+
+#functions for printing eulerian circuit
+def to_edge_dict(edge_list: list) -> dict:
+    """
+    Converts a graph from tuples of edges to dictionary of vertices.
+
+    >>> to_edge_dict([(1,2), (3,4), (1,5), (2,4)])
+    {1: [2, 5], 2: [1, 4], 3: [4], 4: [2, 3], 5: [1]}
+    """
+    result = {}
+    for each in edge_list:
+        if each[0] not in result:
+            result[each[0]] = [each[1]]
+        else:
+            result[each[0]].append(each[1])
+        if each[1] not in result:
+            result[each[1]] = [each[0]]
+        else:
+            result[each[1]].append(each[0])
+
+    for key in result:
+        result[key].sort()
+
+    return result
+
+
+def dfs(u, graph, visited_edge, path=[]):
+    """
+    Uses dfs for finding eulerian path traversal
+    """
+    path = path + [u]
+    for v in graph[u]:
+        if visited_edge[u][v] is False:
+            visited_edge[u][v], visited_edge[v][u] = True, True
+            path = dfs(v, graph, visited_edge, path)
+    return path
+
+
+def check_circuit_or_path(graph, max_node):
+    """
+    Checks whether graph has euler path or circuit
+    """
+    odd_degree_nodes = 0
+    odd_node = -1
+    for i in range(max_node):
+        if i not in graph.keys():
+            continue
+        if len(graph[i]) % 2 == 1:
+            odd_degree_nodes += 1
+            odd_node = i
+    if odd_degree_nodes == 0:
+        return 1, odd_node
+    if odd_degree_nodes == 2:
+        return 2, odd_node
+    return 3, odd_node
+
+
+def check_euler(graph, max_node=10):
+    """
+    Prints the Eulerian circuit or the message about its absence
+
+    >>> check_euler({1: [2, 3, 4], 2: [1, 3], 3: [1, 2], 4: [1, 5], 5: [4]})
+    graph doesn't have an Eulerian circuit
+    >>> check_euler({1: [2, 3, 4, 5], 2: [1, 3], 3: [1, 2], 4: [1, 5], 5: [1, 4]})
+    [1, 2, 3, 1, 4, 5, 1]
+    >>> check_euler({1: [2, 3, 4], 2: [1, 3, 4], 3: [1, 2], 4: [1, 2, 5], 5: [4]})
+    graph doesn't have an Eulerian circuit
+    >>> check_euler({1: [2, 3], 2: [1, 3], 3: [1, 2]})
+    [1, 2, 3, 1]
+    >>> check_euler({1: [], 2: []})
+    [1]
+    """
+    visited_edge = [[False for _ in range(max_node + 1)] for _ in range(max_node + 1)]
+    check, odd_node = check_circuit_or_path(graph, max_node)
+    start_node = 1
+    if check == 1:
+        path = dfs(start_node, graph, visited_edge)
+        print(path)
+    else:
+        print("graph doesn't have an Eulerian circuit")
